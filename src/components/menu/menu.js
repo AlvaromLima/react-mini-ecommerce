8e672057669f65157f37b86540a35c9409d28870
Alvaro Lima <alvaromlima@gmail.com>
2022-07-29 16:31:05 -0300

    Inserido total de produtos no carrinho

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingBasket, faCashRegister, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 
-function Menu() {
+function Menu(props) {
+
+    function calcularTotal() {
+        if (props.produtos.length === 0 ) {
+            return '0,00';
+        }
+        
+        let total = 0;
+        props.produtos.forEach(produto => {
+            //substituindo ',' por '.' e 'R$ ' por nada, no preco do produto que esta na tela.
+            let preco = produto.preco.replace(',', '.').replace('R$ ', '');
+            total += parseFloat(preco) * produto.quantidade;
+        });
+        return total.toFixed(2).toString().replace('.', ',');
+    }
 
     return (
         <Navbar bg="dark" variant="dark">
@@ -19,19 +34,41 @@ function Menu() {
                             </div>
                         }
                         drop="left">
-                        <NavDropdown.Item href="">
+                        <NavDropdown.Item href=""
+                            onClick={props.handleExibirProdutos}>
                             <FontAwesomeIcon icon={faShoppingBasket} />
                             &nbsp;
                             <strong>Produtos</strong>
                         </NavDropdown.Item>
                         <NavDropdown.Divider />
-
+                        {/* ITENS DO CARRINHO */}
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="" data-testid="total-carrinho">
+                            Total: R$ {calcularTotal()}    
+                        </NavDropdown.Item>
+                        <span className={props.produtos.length === 0 ? 'hidden' : null}>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item 
+                                href=""
+                                style={{ color: 'green' }}
+                                onClick={() => props.handleExibirCheckout(calcularTotal())}>
+                                <FontAwesomeIcon icon={faCashRegister} />
+                                &nbsp;
+                                Finalizar compra
+                            </NavDropdown.Item>
+                        </span>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
     );
 
+}
+
+Menu.propTypes = {
+    produtos: PropTypes.array.isRequired,
+    handleExibirProdutos: PropTypes.func.isRequired,
+    handleExibirCheckout: PropTypes.func.isRequired
 }
 
 export default Menu;
